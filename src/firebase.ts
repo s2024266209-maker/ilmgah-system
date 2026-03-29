18a614874d2f134bbf979592737667fe7f94cea3
s2024266209-maker <s2024266209@umt.edu.pk>
2026-03-29 23:00:33 +0500

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,6 +1,7 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth, onAuthStateChanged, User } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
+console.log("Checking API Key:", import.meta.env.VITE_FIREBASE_API_KEY);
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
